004b7ed7dc24603965456441b36f973c09b40260
changxiaokun <939083230@qq.com>
2021-07-03 13:52:15 +0800

    7-3

--- manager.py
+++ manager.py
@@ -1,3 +1,5 @@
+import json
+
 import tornado.web
 import tornado.ioloop
 import tornado.options
@@ -31,11 +33,17 @@ class IndexHandler(tornado.web.RequestHandler):
         self.write('<h3>大家好,我是主页</h3>')
 
     def post(self):
+        # name = self.get_argument('name')
+        # city = self.get_argument('city')
+
+        # 建议使用以下方式
         city = self.get_body_argument('city')
         print(city)
         names = self.get_body_arguments('name')
         print(names)
         self.write('<h3>大家好,我是POST请求方法</h3>')
+        wd = self.get_query_argument('wd')
+        print(wd)
 
     def put(self):
         self.write('<h3>大家好,我是PUT请求方法</h3>')
@@ -44,9 +52,127 @@ class IndexHandler(tornado.web.RequestHandler):
         self.write('<h3>大家好,我是DELETE请求方法</h3>')
 
 
+class SearchHandler(tornado.web.RequestHandler):
+    mapper = {
+        'python': 'python是最流行的AI语言',
+        'java': 'java 也的学啊，很厉害'
+    }
+
+    def get(self):
+        html = """
+            <h3>搜索%s结果</h3>
+            <p>
+                %s
+            </p>
+        """
+        wd = self.get_query_argument('wd')
+        result = self.mapper.get(wd)
+        # self.write(html % (wd, result))
+        resp_data = {
+            'wd': wd,
+            'result': result
+        }
+        # 设置响应头的数据类型
+        self.set_header('Content-Type', 'application/json;charset=utf-8')
+        self.write(json.dumps(resp_data))
+        self.set_status(200)  # 设置响应状态码
+        # cookie操作
+        self.set_cookie('wd', wd)
+
+
+class CookieHandler(tornado.web.RequestHandler):
+    def get(self):
+        # 验证参数中是否存在name?
+        if self.request.arguments.get('name'):
+
+            # 从查询参数中读取Cookie的名称
+            name = self.get_query_argument('name')
+            # 从cookies中获取name的对象或值
+            value = self.get_cookie(name)
+            self.write(value)
+        else:
+            # 查看所有的cookie
+            cookies: dict = self.request.cookies
+            html = """
+                <ul> %s </ul>
+            """
+            lis = []
+            for key in cookies:
+                lis.append('<li>%s : %s </li>' % (key, self.get_cookie(key)))
+            html = '显示所有的cookie' + html % ''.join(lis)
+            html += """
+                <form method="post">
+                    <input name="delete_name" placeholder="请输入cookie的名称">
+                    <button>提交</button>
+                </form>
+            """
+            self.write(html)
+
+    # 这里的post的请求当delete来做，目的是为了删除cookie
+    def post(self):
+        name = self.get_argument('delete_name')
+        if self.request.cookies.get(name, None):
+            # 存在的话
+            self.clear_cookie(name)
+            self.write('删除 %s 成功' % name)
+        else:
+            self.write('删除 %s 失败，不存在！！' % name)
+
+        # 重定向操作时，不需要再调用self.write()
+        self.redirect('/cookie')  # 重定向
+
+
+class OrderHandler(tornado.web.RequestHandler):
+    goods = [
+        {
+            'id': 1,
+            'name': 'Python高级开发',
+            'author': 'cxk',
+            'price': 190
+        },
+        {
+            'id': 2,
+            'name': 'Java高级开发',
+            'author': 'ck',
+            'price': 290
+        },
+    ]
+    action_map = {
+        1: '取消订单',
+        2: '再次购买',
+        3: '评价'
+    }
+
+    def query(self, order_id):
+        for item in self.goods:
+            if item.get('id') == order_id:
+                return item
+
+    def get(self, order_id, action_code):
+        self.write('订单查询')
+        html = """
+            <p>
+                商品编号: %s
+            </p>
+            <p>
+                商品名称: %s
+            </p>
+            <p>
+                商品价格: %s
+            </p>
+        """
+        good = self.query(int(order_id))
+        self.write(html % (good.get('id'), good.get('name'), good.get('price')))
+        # self.write(self.query(int(order_id)))
+        self.write(self.action_map.get(int(action_code)))
+
+
 def make_app():
     return tornado.web.Application([
         ('/', IndexHandler),
+        ('/search', SearchHandler),
+        ('/cookie', CookieHandler),
+        (r'/order/(?P<action_code>\d+)/(?P<order_id>\d+)', OrderHandler)
     ], default_host=tornado.options.options.host)
 
 
--- test_method.py
+++ test_method.py
@@ -5,11 +5,34 @@ import requests
 class TestTornadoRequest(TestCase):
     base_url = 'http://localhost:8000'
 
-    def test_index_post(self):
+    def test_index_get(self):
         url = self.base_url + '/'
+        # params表示查询参数
+        resp = requests.get(url, params={
+            'wd': 'ck',
+            'title': '山西'
+        })
+        print(resp.text)
+
+    def test_index_post(self):
+        url = self.base_url + '/?wd=python'
         # 发起post请求，表单参数使用data来指定
         resp = requests.post(url, data={
             'name': ['ck', 'cxk'],
             'city': '山西'
+        })
+        print(resp.text)
+
+
+class TestCookieRequest(TestCase):
+    url = 'http://localhost:8000/cookie'
+
+    def test_get(self):
+        resp = requests.get(self.url)
+        print(resp.text)
+
+    def test_delete(self):
+        resp = requests.delete(self.url, params={
+            'name': 'token'
         })
         print(resp.text)
