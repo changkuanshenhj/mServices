896fcea15c83b42c400b6a4932d39dbc221658c7
changxiaokun <939083230@qq.com>
2021-07-04 23:08:54 +0800

    7-4（晚）

--- api_server.py
+++ api_server.py
@@ -6,6 +6,7 @@ from tornado.options import options, define, parse_command_line
 
 class LoginHandler(RequestHandler):
     def get(self):
+        print('asdasdasd')
         self.write('login_get')
 
     def post(self):
@@ -20,17 +21,18 @@ class LoginHandler(RequestHandler):
 
 def make_app():
     return Application(
-        handles=[
-        ('/login', LoginHandler),
-    ], default_host=options.h)
+        handlers=[
+            ('/login', LoginHandler),
+        ],
+        default_host=options.h)
 
 
 if __name__ == '__main__':
     define('p', default=8000, type=int, help='绑定的port端口')
     define('h', default='localhost', type=str, help='绑定的主机ip')
-    parse_command_line() # 解释命令行参数
+    parse_command_line()  # 解释命令行参数
 
     app = make_app()
-    app.listen(options.port)
-
+    app.listen(options.p)
+    print('starting http://%s:%s' % (options.h, options.p))
     IOLoop.current().start()
